34c90894b21fd00bb6cd190c7c8f81eade30f20f
shahbazee <shahbazbutt22ee@gmail.com>
2026-09-16 09:44:59 +0500

    Configure PostgreSQL SSL for deployment

--- elearning_site/settings.py
+++ elearning_site/settings.py
@@ -116,9 +116,11 @@ DATABASES = {
         'PASSWORD': os.environ.get("DB_PASSWORD", ""),
         'HOST': os.environ.get("DB_HOST", "localhost"),
         'PORT': os.environ.get("DB_PORT", "5432"),
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
-
 
 # Password validation
 
